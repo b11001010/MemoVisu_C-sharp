e42037dd90a6f92938d9dec64b6db114669ab990
Bando <ppoponda@gmail.com>
2016-01-08 04:16:10 +0900

    first commit

---
+++ MemoVisu_Form/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace MemoVisu_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button_paint = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.mainPictureBox = new System.Windows.Forms.PictureBox();
+            this.picture_map = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_map)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(471, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 19);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(430, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(471, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 19);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(430, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(691, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(88, 124);
+            this.listBox1.TabIndex = 5;
+            // 
+            // button_paint
+            // 
+            this.button_paint.Location = new System.Drawing.Point(12, 12);
+            this.button_paint.Name = "button_paint";
+            this.button_paint.Size = new System.Drawing.Size(75, 23);
+            this.button_paint.TabIndex = 6;
+            this.button_paint.Text = "描画";
+            this.button_paint.UseVisualStyleBackColor = true;
+            this.button_paint.Click += new System.EventHandler(this.button_paint_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.mainPictureBox);
+            this.panel1.Location = new System.Drawing.Point(12, 148);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(687, 543);
+            this.panel1.TabIndex = 7;
+            // 
+            // mainPictureBox
+            // 
+            this.mainPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.mainPictureBox.Name = "mainPictureBox";
+            this.mainPictureBox.Size = new System.Drawing.Size(453, 307);
+            this.mainPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.mainPictureBox.TabIndex = 0;
+            this.mainPictureBox.TabStop = false;
+            // 
+            // picture_map
+            // 
+            this.picture_map.Location = new System.Drawing.Point(715, 148);
+            this.picture_map.Name = "picture_map";
+            this.picture_map.Size = new System.Drawing.Size(64, 543);
+            this.picture_map.TabIndex = 8;
+            this.picture_map.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 703);
+            this.Controls.Add(this.picture_map);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button_paint);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_map)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button_paint;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox mainPictureBox;
+        private System.Windows.Forms.PictureBox picture_map;
+    }
+}
+
---
+++ MemoVisu_Form/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoVisu_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_paint_Click(object sender, EventArgs e)
+        {
+            //200x100サイズのImageオブジェクトを作成する
+            Bitmap img = new Bitmap(500, 1000);
+
+            //ImageオブジェクトのGraphicsオブジェクトを作成する
+            Graphics g = Graphics.FromImage(img);
+
+            //全体を黒で塗りつぶす
+            g.FillRectangle(Brushes.Black, g.VisibleClipBounds);
+            //黄色い扇形を描画する
+            g.DrawPie(Pens.Yellow, 60, 10, 80, 80, 30, 300);
+
+            //リソースを解放する
+            g.Dispose();
+
+            mainPictureBox.SizeMode = PictureBoxSizeMode.AutoSize;
+
+            //作成した画像を表示する
+            mainPictureBox.Image = img;
+
+
+            //描画先とするImageオブジェクトを作成する
+            Bitmap canvas = new Bitmap(picture_map.Width, picture_map.Height);
+            //ImageオブジェクトのGraphicsオブジェクトを作成する
+            Graphics g2 = Graphics.FromImage(canvas);
+            
+            //画像のサイズを縮小してcanvasに描画する
+            g2.DrawImage(img, 0, 0, picture_map.Width, picture_map.Height);
+            //Imageオブジェクトのリソースを解放する
+
+            //Graphicsオブジェクトのリソースを解放する
+            g2.Dispose();
+            //PictureBox1に表示する
+            picture_map.Image = canvas;
+        }
+    }
+}
