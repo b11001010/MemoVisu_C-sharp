6aeefd798f49e2c199368c4d842a4f4f8dfb15ac
Bando <ppoponda@gmail.com>
2016-01-08 17:53:52 +0900

    OllyDbgの実行トレースログを読み込んで実行領域を可視化する処理を実装

--- MemoVisu_Form/Form1.Designer.cs
+++ MemoVisu_Form/Form1.Designer.cs
@@ -28,51 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.width_textBox = new System.Windows.Forms.TextBox();
+            this.width_label = new System.Windows.Forms.Label();
+            this.height_textBox = new System.Windows.Forms.TextBox();
+            this.height_label = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button_paint = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.mainPictureBox = new System.Windows.Forms.PictureBox();
             this.picture_map = new System.Windows.Forms.PictureBox();
+            this.file_open = new System.Windows.Forms.Button();
+            this.margin_textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picture_map)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // width_textBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(471, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 19);
-            this.textBox1.TabIndex = 1;
+            this.width_textBox.Location = new System.Drawing.Point(471, 12);
+            this.width_textBox.Name = "width_textBox";
+            this.width_textBox.Size = new System.Drawing.Size(100, 19);
+            this.width_textBox.TabIndex = 1;
             // 
-            // label1
+            // width_label
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(430, 15);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 12);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.width_label.AutoSize = true;
+            this.width_label.Location = new System.Drawing.Point(430, 15);
+            this.width_label.Name = "width_label";
+            this.width_label.Size = new System.Drawing.Size(32, 12);
+            this.width_label.TabIndex = 2;
+            this.width_label.Text = "width";
             // 
-            // textBox2
+            // height_textBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(471, 38);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 19);
-            this.textBox2.TabIndex = 3;
+            this.height_textBox.Location = new System.Drawing.Point(471, 38);
+            this.height_textBox.Name = "height_textBox";
+            this.height_textBox.Size = new System.Drawing.Size(100, 19);
+            this.height_textBox.TabIndex = 3;
             // 
-            // label2
+            // height_label
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(430, 41);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 12);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
+            this.height_label.AutoSize = true;
+            this.height_label.Location = new System.Drawing.Point(430, 41);
+            this.height_label.Name = "height_label";
+            this.height_label.Size = new System.Drawing.Size(36, 12);
+            this.height_label.TabIndex = 4;
+            this.height_label.Text = "height";
             // 
             // listBox1
             // 
@@ -85,7 +88,7 @@
             // 
             // button_paint
             // 
-            this.button_paint.Location = new System.Drawing.Point(12, 12);
+            this.button_paint.Location = new System.Drawing.Point(12, 113);
             this.button_paint.Name = "button_paint";
             this.button_paint.Size = new System.Drawing.Size(75, 23);
             this.button_paint.TabIndex = 6;
@@ -95,6 +98,9 @@
             // 
             // panel1
             // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.AutoScroll = true;
             this.panel1.Controls.Add(this.mainPictureBox);
             this.panel1.Location = new System.Drawing.Point(12, 148);
@@ -104,34 +110,67 @@
             // 
             // mainPictureBox
             // 
+            this.mainPictureBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.mainPictureBox.Location = new System.Drawing.Point(0, 0);
             this.mainPictureBox.Name = "mainPictureBox";
-            this.mainPictureBox.Size = new System.Drawing.Size(453, 307);
+            this.mainPictureBox.Size = new System.Drawing.Size(684, 540);
             this.mainPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.mainPictureBox.TabIndex = 0;
             this.mainPictureBox.TabStop = false;
             // 
             // picture_map
             // 
+            this.picture_map.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picture_map.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.picture_map.Location = new System.Drawing.Point(715, 148);
             this.picture_map.Name = "picture_map";
-            this.picture_map.Size = new System.Drawing.Size(64, 543);
+            this.picture_map.Size = new System.Drawing.Size(52, 540);
             this.picture_map.TabIndex = 8;
             this.picture_map.TabStop = false;
+            // 
+            // file_open
+            // 
+            this.file_open.Location = new System.Drawing.Point(12, 12);
+            this.file_open.Name = "file_open";
+            this.file_open.Size = new System.Drawing.Size(75, 23);
+            this.file_open.TabIndex = 9;
+            this.file_open.Text = "ファイルを開く";
+            this.file_open.UseVisualStyleBackColor = true;
+            this.file_open.Click += new System.EventHandler(this.file_open_Click);
+            // 
+            // margin_textBox
+            // 
+            this.margin_textBox.Location = new System.Drawing.Point(471, 95);
+            this.margin_textBox.Name = "margin_textBox";
+            this.margin_textBox.Size = new System.Drawing.Size(100, 19);
+            this.margin_textBox.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(430, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "EDI";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(791, 703);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.margin_textBox);
+            this.Controls.Add(this.file_open);
             this.Controls.Add(this.picture_map);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button_paint);
             this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.height_label);
+            this.Controls.Add(this.height_textBox);
+            this.Controls.Add(this.width_label);
+            this.Controls.Add(this.width_textBox);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -145,15 +184,18 @@
         }
 
         #endregion
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox width_textBox;
+        private System.Windows.Forms.Label width_label;
+        private System.Windows.Forms.TextBox height_textBox;
+        private System.Windows.Forms.Label height_label;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button_paint;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox mainPictureBox;
         private System.Windows.Forms.PictureBox picture_map;
+        private System.Windows.Forms.Button file_open;
+        private System.Windows.Forms.TextBox margin_textBox;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- MemoVisu_Form/Form1.cs
+++ MemoVisu_Form/Form1.cs
@@ -7,11 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Collections;
 
 namespace MemoVisu_Form
 {
     public partial class Form1 : Form
     {
+
+        int edi;
+
+        ArrayList eips = new ArrayList();
+        int margin_x = 0;
+        int margin_y = 0;
+        int height = 3;
+        int width = 3;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,21 +30,35 @@ namespace MemoVisu_Form
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            width_textBox.Text = width.ToString();
+            height_textBox.Text = height.ToString();
         }
 
         private void button_paint_Click(object sender, EventArgs e)
         {
+
+            margin_textBox.Text = edi.ToString();
+
+            int height = int.Parse(width_textBox.Text);
+            int width = int.Parse(height_textBox.Text);
+
             //200x100サイズのImageオブジェクトを作成する
-            Bitmap img = new Bitmap(500, 1000);
+            Bitmap img = new Bitmap(266*width, 1000);
 
             //ImageオブジェクトのGraphicsオブジェクトを作成する
             Graphics g = Graphics.FromImage(img);
 
-            //全体を黒で塗りつぶす
-            g.FillRectangle(Brushes.Black, g.VisibleClipBounds);
-            //黄色い扇形を描画する
-            g.DrawPie(Pens.Yellow, 60, 10, 80, 80, 30, 300);
+            int x, y;
+            foreach (int eip in eips) {
+                int pos = eip - 0x3C0000;	//オフセット
+                x = pos % 0x100;
+                y = pos / 0x100;
+                x = x * margin_x + (x - 1) * width + 10;
+                y = y * margin_y + (y - 1) * height;
+                //塗りつぶされた長方形を描画する
+                Brush b = new SolidBrush(Color.FromArgb(10, Color.Red));
+                g.FillRectangle(b, x, y, width, height);
+            }
 
             //リソースを解放する
             g.Dispose();
@@ -49,7 +74,7 @@ namespace MemoVisu_Form
             //ImageオブジェクトのGraphicsオブジェクトを作成する
             Graphics g2 = Graphics.FromImage(canvas);
             
-            //画像のサイズを縮小してcanvasに描画する
+            //画像のサイズを2倍にしてcanvasに描画する
             g2.DrawImage(img, 0, 0, picture_map.Width, picture_map.Height);
             //Imageオブジェクトのリソースを解放する
 
@@ -57,6 +82,64 @@ namespace MemoVisu_Form
             g2.Dispose();
             //PictureBox1に表示する
             picture_map.Image = canvas;
+        }
+
+        //「ファイルを開く」ボタンをクリック
+        private void file_open_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialogクラスのインスタンスを作成
+            OpenFileDialog ofd = new OpenFileDialog();
+            
+            //はじめに「ファイル名」で表示される文字列を指定する
+            ofd.FileName = "rtrace.txt";
+            //はじめに表示されるフォルダを指定する
+            ofd.InitialDirectory = @"";
+            //[ファイルの種類]に表示される選択肢を指定する
+            //指定しないとすべてのファイルが表示される
+            ofd.Filter =
+                "テキストファイル(*.txt)|*.txt|すべてのファイル(*.*)|*.*";
+            //[ファイルの種類]ではじめに
+            //「すべてのファイル」が選択されているようにする
+            ofd.FilterIndex = 1;
+            //タイトルを設定する
+            ofd.Title = "開くファイルを選択してください";
+            //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+            ofd.RestoreDirectory = true;
+
+            //ダイアログを表示する
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //OKボタンがクリックされたとき
+                //選択されたファイルを読み取り専用で開く
+                Stream stream;
+                stream = ofd.OpenFile();
+                if (stream != null)
+                {
+                    //内容を読み込み、表示する
+                    string line = "";
+                    StreamReader sr = new StreamReader(stream);
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        try
+                        {
+                            //EIPをリストに格納
+                            string eipString = line.Substring(0, 8);
+                            eips.Add(Convert.ToInt32(eipString, 16));
+
+                            //レジスタの値を更新
+                            string regString = line.Substring(line.Length - 12, 12);
+                            if ("EDI" == regString.Substring(0,3))
+                            {
+                                edi = Convert.ToInt32(regString.Substring(regString.Length - 8, 8), 16);
+                            }
+                        }
+                        catch {/* nothing */}
+                    }
+                    //閉じる
+                    sr.Close();
+                    stream.Close();
+                }
+            }
         }
     }
 }
